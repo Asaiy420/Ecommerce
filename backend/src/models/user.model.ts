28c03653fbd4294159b3cdd59c747d145bd1b19c
Asaiy420 <kcaryan05@gmail.com>
2025-05-19 18:59:53 +0545

    added pre-hooks

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,51 +1,83 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-interface IUser extends mongoose.Document{
-    username: string
-    email:string
-    password:string
-    cartItems: Array<any>
-    profilePicture:string
-    createdAt: Date
-    updatedAt: Date
+interface IUser extends mongoose.Document {
+  username: string;
+  email: string;
+  password: string;
+  cartItems: Array<any>;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(password: string): Promise<boolean>;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: [3, "Username must be at least 3 characters long"],
-        maxlength: [20, "Username must be less than 20 characters long"],
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [3, "Username must be at least 3 characters long"],
+      maxlength: [20, "Username must be less than 20 characters long"],
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            "Please enter a valid email address"
-        ],
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
     },
     password: {
-        type: String,
-        required: true,
-        minlength: [6, "Password must be at least 8 characters long"],
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 8 characters long"],
     },
-    cartItem: [
-        {
-            quanitity: {
-                type: Number,
-                default: 1,
-            },
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref:"Product",
-            }
-        }
-    ]
-}, {timestamps: true});
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
 
+// pre save hook to hash the password before saving to the database
+
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next(); // if password is not modified, skip the hashing
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+// method to compare the password with the hashed password
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
