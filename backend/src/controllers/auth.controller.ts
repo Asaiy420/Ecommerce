c286186090ea52715358d347c9846c19c75ac169
Asaiy420 <kcaryan05@gmail.com>
2025-05-20 13:17:12 +0545

    added signup controller and fixed port issue in the server

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,33 @@
 import {Request, Response} from "express";
+import User from "../models/user.model.js";
 
 
 export const signUp = async (req: Request, res: Response): Promise<void> =>{
-    
+    try {
+        const {username, email, password} = req.body;
+
+        if (!username || !email || !password) {
+            res.status(400).json({message: "All fields are required"});
+            return;
+        };
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser){
+            res.status(400).json({message: "User with this email already exists"});
+            return;
+        }
+
+        const user = await User.create({username, email, password});
+
+        res.status(201).json({message: "User created successfully", user})
+        
+
+    } catch (error:any) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal server error"});
+        return;
+    }
 }
 
 export const login = async (req: Request, res: Response): Promise<void> =>{
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      minlength: [6, "Password must be at least 8 characters long"],
+      minlength: [6, "Password must be at least 6 characters long"],
     },
     cartItems: [
       {
