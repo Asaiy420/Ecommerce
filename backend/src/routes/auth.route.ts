b36949178c6de6bc00ed6d438fefc4bb00b176f6
Asaiy420 <kcaryan05@gmail.com>
2025-05-20 13:18:00 +0545

    added missing .js in import

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { logout, signUp } from "../controllers/auth.controller";
-import { login } from "../controllers/auth.controller";
+import { logout, signUp } from "../controllers/auth.controller.js";
+import { login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
