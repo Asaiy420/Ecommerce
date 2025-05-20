16509c891da8f7585d4a2bbb609bd188bdafc75c
Asaiy420 <kcaryan05@gmail.com>
2025-05-20 15:49:08 +0545

    added cookie parser and ts types

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port 3001`);
   connectDB();
 });
