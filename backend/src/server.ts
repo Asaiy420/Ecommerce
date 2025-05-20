0b16f18e1a0d50d6854d626c3d9655bc2f6e425f
Asaiy420 <kcaryan05@gmail.com>
2025-05-20 13:17:33 +0545

    fixed port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,19 +2,18 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.route";
-import { connectDB } from "./config/db";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3001, () => {
+  console.log(`Server is running on port 3001`);
   connectDB();
 });
-
