61ddfc35243f7db49ce357176ba452f69de0b304
Asaiy420 <kcaryan05@gmail.com>
2025-05-21 12:47:15 +0545

    added products api route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,10 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port 3001`);
