f6da3d2f6cce8379c8c5c3867418c23abd6d88ef
Asaiy420 <kcaryan05@gmail.com>
2025-05-22 13:55:34 +0545

    improved delete logic

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -53,23 +53,26 @@ export const createProduct = async (
 
     let cloudinaryResponse = null;
 
-    if (image){
-       cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: "products"})
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
     }
 
     if (!name || !price || !description || !image || !category) {
       res.status(400).json({ message: "Missing required fields" });
-      return; 
+      return;
     }
 
     const product = await Product.create({
       name,
       price,
       description,
-      image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
-      category
-
-    })
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
 
     await product.save();
 
@@ -141,6 +144,17 @@ export const deleteProduct = async (
       return;
     }
 
+    if (existingProduct.image) {
+      const publicId = existingProduct.image.split("/").pop()?.split(".")[0]; // will get the id of the image
+
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+        console.log("deleted image from cloudinary")
+      } catch (error: any) {
+        console.error("Error when deleting the image ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    }
     const deletedProduct = await Product.findByIdAndDelete(id);
 
     res.status(200).json({
@@ -153,29 +167,30 @@ export const deleteProduct = async (
   }
 };
 
-export const getFeaturedProducts = async (req: Request, res: Response):Promise<void> => {
-  try{
+export const getFeaturedProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const cachedKey = await redis.get("featured_products");
 
-    const cachedKey = await redis.get("featured_products")
-
-    if (cachedKey){  
-      res.json(JSON.parse(cachedKey))
+    if (cachedKey) {
+      res.json(JSON.parse(cachedKey));
       return;
     }
 
-    const featuredProducts = await Product.find({isFeatured:true}).lean()
+    const featuredProducts = await Product.find({ isFeatured: true }).lean();
 
-    if (!featuredProducts){
-      res.status(404).json({error: "NO featured products found"})
+    if (!featuredProducts) {
+      res.status(404).json({ error: "NO featured products found" });
       return;
     }
 
     await redis.set("featured_products", JSON.stringify(featuredProducts));
 
     res.json(featuredProducts);
-
-  }catch(error:any){
+  } catch (error: any) {
     console.log("Error in getFeatured controller", error.message);
-    res.status(500).json({message: "Internal Server Error"})
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
