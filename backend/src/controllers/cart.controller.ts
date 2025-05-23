71085e6cb2032daaf418f2a42ccf53f54acacb8a
Asaiy420 <kcaryan05@gmail.com>
2025-05-23 15:56:11 +0545

    add: feat  fetch cart products

--- backend/src/controllers/cart.controller.ts
+++ backend/src/controllers/cart.controller.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express";
+import Product from "../models/product.model.js";
 
 export const getCartProducts = async (
   req: Request,
   res: Response
-): Promise<void> => {
+): Promise<any> => {
   try {
-  } catch (error: any) {}
+    const products = await Product.find({
+      _id: { $in: (req as any).user.cartItems },
+    });
+    const cartItems = products.map((product) => {
+      const item = (req as any).user.cartItems.find(
+        (cartItem: any) => cartItem.id === product.id
+      );
+      return { ...product.toJSON(), quantity: item.quantity };
+    });
+    return res.status(200).json(cartItems);
+  } catch (error: any) {
+    console.log("Error when getting cart products", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const addToCart = async (req: Request, res: Response): Promise<void> => {
@@ -39,7 +53,35 @@ export const updateQuantity = async (
   res: Response
 ): Promise<void> => {
   try {
-  } catch (error: any) {}
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = (req as any).user;
+
+    const existingItem = user.cartItems.find(
+      (item: any) => item.id === productId
+    );
+
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter(
+          (item: any) => item.id !== productId
+        );
+        await user.save();
+        res.json(user.cartItems);
+        return;
+      }
+
+      existingItem.quantity = quantity;
+      await user.save();
+      res.json(user.cartItems);
+      return;
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error: any) {
+    console.log("Error in update cart controller", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const deleteAllFromCart = async (
