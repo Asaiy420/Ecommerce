06c5f5f33034ed925eeeb43addad3c51aaae8aca
Asaiy420 <kcaryan05@gmail.com>
2025-05-25 16:31:07 +0545

    feat: signup page

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,10 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import { motion } from "framer-motion";
 
 const SignUp = () => {
+  const loading = true;
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">This is a signup page</h1>
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: -40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-shadow-white">
+          Create your account
+        </h2>
+      </motion.div>
+
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="bg-black py-8 px-4 shadow sm-rounded-lg sm:px-10 ">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-white"
+              >
+                Full Name
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-white" aria-hidden="true" />
+                </div>
+                <input 
+                  id="name"
+                  type="text" 
+                  required
+                  value={formData.name}
+                  onChange={(e) => setFormData({...formData, name: e.target.value})}
+                  />
+              </div>
+            </div>
+          </form>
+        </div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
 export default SignUp;
