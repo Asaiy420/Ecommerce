3cb03e4a9a8bfa33812386031957ab4a1395e161
Asaiy420 <kcaryan05@gmail.com>
2025-05-26 17:35:39 +0545

    add: feat use Effect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./store/useUserStore";
+import { useEffect } from "react";
 
 const App = () => {
+
+  const { user, checkAuth } = useUserStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
   return (
     <div className="min-h-screen bg-black text-white relative overflow-hidden">
       <div className="relative z-50 pt-20">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={ !user? <SignUp/>: <Navigate to="/" />} />
+          <Route path="/login" element={ !user? <Login/>: <Navigate to="/" />} />
         </Routes>
       </div>
       <Toaster/>
