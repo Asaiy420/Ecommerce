ce876b06091a92e1baeac4dbf53f58f29ef37823
Asaiy420 <kcaryan05@gmail.com>
2025-05-26 17:54:42 +0545

    add: (chore) color

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-zinc-950">
+    <div className="flex items-center justify-center min-h-screen bg-black">
       <div className="relative">
         <div className="size-20 border-white border-2 rounded-full">
           <div className="size-20 border-emerald-500 border-t-2  animate-spin rounded-full absolute left-0 top-0" />
