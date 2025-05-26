e5f56df80eb67b6b2d3bb7c9a64fb3651f855202
Asaiy420 <kcaryan05@gmail.com>
2025-05-26 20:16:01 +0545

    add: (feat) Home Page UI

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,38 @@
+import CategoryItem from "../components/CategoryItem";
 
 const Home = () => {
+  const categories = [
+    { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
+    { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+    { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+    { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+    { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+    { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+    { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+  ];
   return (
-    <div>
-        <h1 className="text-3xl font-bold underline">THis is a home page</h1>
-    </div>
-  )
-}
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-18">
+        <h1 className="text-center text-5xl sm:text-6xl font-bold mb-4">
+          Explore Categories
+        </h1>
+        <p className="text-center text-xl text-white mb-12">
+          Discover the latest trends and styles from our extensive collection of
+          products.
+        </p>
 
-export default Home
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {categories.map(category => (
+            <CategoryItem
+              category={category}
+              key={category.name}
+            />
+          ))}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
