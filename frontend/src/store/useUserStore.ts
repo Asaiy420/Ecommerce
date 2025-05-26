31e0fe2fcbce7af038feec0e6f5706294e466b9e
Asaiy420 <kcaryan05@gmail.com>
2025-05-26 17:34:58 +0545

    add : feat check authentication

--- frontend/src/store/useUserStore.ts
+++ frontend/src/store/useUserStore.ts
@@ -28,6 +28,7 @@ interface UserStore {
   checkingAuth: boolean;
   signup: (params: SignupParams) => Promise<void>;
   login: (params: LoginParams) => Promise<void>;
+  checkAuth: () => Promise<void>;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
@@ -79,7 +80,22 @@ export const useUserStore = create<UserStore>((set) => ({
       const res = await axiosInstance.post("/auth/login", { email, password });
 
       set({ user: res.data, loading: false });
-      
+    } catch (error) {
+      set({ loading: false });
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data?.message || "An error occurred");
+      } else {
+        toast.error("An unexpected error occurred");
+      }
+    }
+  },
+
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+
+    try {
+      const response = await axiosInstance.get("/auth/profile");
+      set({ user: response.data, checkingAuth: false });
     } catch (error) {
       set({ loading: false });
       if (error instanceof AxiosError) {
