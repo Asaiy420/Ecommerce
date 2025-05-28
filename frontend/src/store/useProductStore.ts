4c97221d524c111f7fce9987d2999e942d686753
Asaiy420 <kcaryan05@gmail.com>
2025-05-28 15:10:17 +0545

    add : (feat) fetch product function

--- frontend/src/store/useProductStore.ts
+++ frontend/src/store/useProductStore.ts
@@ -4,7 +4,7 @@ import axiosInstance from "../lib/axios";
 import { AxiosError } from "axios";
 
 interface Product {
-    _id: string;
+  _id: string;
   name: string;
   description: string;
   price: number;
@@ -27,9 +27,9 @@ interface ProductStore {
   setProducts: (products: Product[]) => void;
   createProduct: (productData: CreateProductInput) => Promise<void>;
   fetchAllProducts: () => Promise<void>;
-  updateProduct: (id:string,productData: CreateProductInput) => Promise<void>;
-  deleteProduct: (id:string) => Promise<void>;
-  toggleFeaturedProduct: (id:string) => Promise<void>;
+  fetchProductByCategory: (category: string) => Promise<void>;
+  deleteProduct: (id: string) => Promise<void>;
+  toggleFeaturedProduct: (id: string) => Promise<void>;
 }
 
 export const useProductStore = create<ProductStore>((set) => ({
@@ -58,52 +58,83 @@ export const useProductStore = create<ProductStore>((set) => ({
       }
     }
   },
- 
+
   fetchAllProducts: async () => {
-    set({loading:true});
+    set({ loading: true });
 
     try {
-        const res = await axiosInstance.get("/products");
-        set({products: res.data.products, loading:false})
+      const res = await axiosInstance.get("/products");
+      set({ products: res.data.products, loading: false });
     } catch (error) {
-        if (error instanceof AxiosError){
-            toast.error(error.response?.data?.message || "An error occurred");
-            set({loading:false})
-        }else{
-            toast.error("An unexpected error occurred when fetching products");
-            set({loading:false})
-        }
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data?.message || "An error occurred");
+        set({ loading: false });
+      } else {
+        toast.error("An unexpected error occurred when fetching products");
+        set({ loading: false });
+      }
     }
   },
 
-  updateProduct: async (id: string, productData: CreateProductInput) => {
-    set({loading:true})
-    
-  },
-
-  deleteProduct: async (id: string) => {
-    set({loading: true})
-    try{
-        await axiosInstance.delete(`/products/${id}`);
-        set((prevState) => ({
-            products: prevState.products.filter((product) => product._id !== id, ),
-            loading:false
-        }))
-
-    }catch(error){
-        if (error instanceof AxiosError){
-            toast.error(error.response?.data?.message || "An error occurred");
-            set({loading: false})
-        }else{
-            toast.error("An unexpected error occurred when deleting products");
-            set({loading: false})
-        }
+  deleteProduct: async (productId: string) => {
+    set({ loading: true });
+    try {
+      await axiosInstance.delete(`/products/${productId}`);
+      set((prevState) => ({
+        products: prevState.products.filter(
+          (product) => product._id !== productId
+        ),
+        loading: false,
+      }));
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data?.message || "An error occurred");
+        set({ loading: false });
+      } else {
+        toast.error("An unexpected error occurred when deleting products");
+        set({ loading: false });
+      }
     }
   },
 
-  toggleFeaturedProduct: async (id: string) => {
-    set({loading: true})
+  toggleFeaturedProduct: async (productId: string) => {
+    set({ loading: true });
+    try {
+      const res = await axiosInstance.patch(`/products/${productId}`);
+
+      set((prevProduct) => ({
+        products: prevProduct.products.map((product) =>
+          product._id === productId
+            ? { ...product, isFeatured: res.data.isFeatured }
+            : product
+        ),
+      }));
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data?.message || "An error occurred");
+        set({ loading: false });
+      } else {
+        toast.error(
+          "An unexpected error occurred when toggling featured products"
+        );
+        set({ loading: false });
+      }
+    }
   },
 
+  fetchProductByCategory: async (category: string) => {
+    set({ loading: true });
 
+    try {
+      const res = await axiosInstance.get(`/products/category/${category}`);
+      set({ products: res.data.products, loading: false });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(
+          error.response?.data?.message || "An error when fetching the product"
+        );
+        set({ loading: false });
+      }
+    }
+  },
 }));
