2855112d6caa278f6a2434cddfca70b5db9bc051
Asaiy420 <kcaryan05@gmail.com>
2025-05-31 09:59:34 +0545

    improved ui

--- frontend/src/components/OrderSummary.tsx
+++ frontend/src/components/OrderSummary.tsx
@@ -23,7 +23,9 @@ const OrderSummary = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <p className="text-xl font-semibold text-white">Order Summary</p>
+      <p className="text-xl font-semibold text-white text-center">
+        Order Summary
+      </p>
 
       <div className="space-y-4">
         <dl className="flex items-center justify-between gap-4">
@@ -62,7 +64,7 @@ const OrderSummary = () => {
       </div>
 
       <motion.button
-        className="flex w-full items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300"
+        className="flex w-full items-center justify-center rounded-lg cursor-pointer bg-emerald-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300"
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
       >
