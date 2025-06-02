ba169239e78811aaa7e19b44228f1750cd5d0a6d
Asaiy420 <kcaryan05@gmail.com>
2025-06-02 10:22:37 +0545

    add: (feat) analytics chart

--- frontend/src/components/AnalyticsTab.tsx
+++ frontend/src/components/AnalyticsTab.tsx
@@ -1,5 +1,96 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import axiosInstance from "../lib/axios";
+import LoadingSpinner from "./LoadingSpinner";
+
+interface AnalyticsCardProps {
+  title: string;
+  value: string | number;
+  color: string;
+}
+
+interface AnalyticsData {
+  users: number;
+  products: number;
+  totalSales: number;
+  totalRevenue: number;
+}
+
 const AnalyticsTab = () => {
-  return <div>AnalyticsTab</div>;
+  const [analyticsData, setAnalyticsData] = useState<AnalyticsData>({
+    users: 0,
+    products: 0,
+    totalSales: 0,
+    totalRevenue: 0,
+  });
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [dailySalesData, setDailySalesData] = useState([]);
+
+  useEffect(() => {
+    const fetchAnalyticsData = async () => {
+      try {
+        const res = await axiosInstance.get("/analytics");
+        setAnalyticsData(res.data.analyticsData);
+        setDailySalesData(res.data.dailySalesData);
+      } catch (error) {
+        console.log("Error in fetchAnalyticsData", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchAnalyticsData();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+        <AnalyticsCard
+          title="Total Users"
+          value={analyticsData.users.toLocaleString()}
+          color="from-emerald-500 to-teal-700"
+        />
+        <AnalyticsCard
+          title="Products"
+          value={analyticsData.products.toLocaleString()}
+          color="from-emerald-500 to-gray-700"
+        />
+        <AnalyticsCard
+          title="Total Sales"
+          value={analyticsData.totalSales.toLocaleString()}
+          color="from-emerald-500 to-cyan-700"
+        />
+        <AnalyticsCard
+          title="Total Revenue"
+          value={`$${analyticsData.totalRevenue.toLocaleString()}`}
+          color="from-emerald-500 to-lime-700"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsTab;
+
+const AnalyticsCard = ({ title, value, color }: AnalyticsCardProps) => (
+  <motion.div
+    className="bg-zinc-950 rounded-lg p-6 shadow-lg overflow-hidden relative"
+    initial={{ opacity: 0, y: -40 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+  >
+    <div className="flex justify-between">
+      <div className="z-10">
+        <p className="text-emerald-300 text-sm mb-1 font-semibold">{title}</p>
+        <h3 className="text-white text-3xl font-bold">{value}</h3>
+      </div>
+    </div>
+    <div className={`absolute inset-0 bg-gradient-to-br ${color} opacity-30`}>
+      <div className="absolute -bottom-4 -right-4 opacity-50"></div>
+    </div>
+  </motion.div>
+);
