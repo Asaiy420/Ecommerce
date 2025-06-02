bdbe4e7ceed02c1a12cade5b808122c98c4e03fa
Asaiy420 <kcaryan05@gmail.com>
2025-06-02 10:22:24 +0545

    fix: bug response not working

--- backend/src/controllers/analytics.controller.ts
+++ backend/src/controllers/analytics.controller.ts
@@ -9,11 +9,11 @@ export const getAnalytics = async (
 ): Promise<void> => {
   try {
     const analyticsData = await getAnalyticsData();
-
     const endDate = new Date();
     const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
-
     const dailySalesData = await getDailySalesData(startDate, endDate);
+
+    res.json({ analyticsData, dailySalesData });
   } catch (error: any) {
     console.log("Error in getAnalytics controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
