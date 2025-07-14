e62d720047f0e990c124c77227a8b1e22de383c3
Asaiy420 <kcaryan05@gmail.com>
2025-07-14 12:17:52 +0545

    fix: update categories and headings to reflect musical instruments

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,23 +2,22 @@ import CategoryItem from "../components/CategoryItem";
 
 const Home = () => {
   const categories = [
-    { href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
-    { href: "/t-shirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
-    { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },  
-    { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
-    { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
-    { href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
+    { href: "/guitar", name: "Guitar", imageUrl: "/jeans.jpg" },
+    { href: "/ukelele", name: "Ukelele", imageUrl: "/tshirts.jpg" },
+    { href: "/e-guitar", name: "Electric Guitar", imageUrl: "/shoes.jpg" },  
+    { href: "/guitar-picks", name: "Guitar Picks", imageUrl: "/glasses.png" },
+    { href: "/guitar-bag", name: "Guitar Bag", imageUrl: "/jackets.jpg" },
+    { href: "/guitar-belt", name: "Guitar Belt", imageUrl: "/suits.jpg" },
     { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
   ];
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-18">
         <h1 className="text-center text-5xl sm:text-6xl font-bold mb-4">
-          Explore Categories
+          Explore The Instruments
         </h1>
         <p className="text-center text-xl text-white mb-12">
-          Discover the latest trends and styles from our extensive collection of
-          products.
+          Discover Your Music Vibe
         </p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
